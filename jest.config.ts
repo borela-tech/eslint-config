6329c516829320c79906436a25fafc2892fdb67d
Alexandre Borela <alexandre@borela.tech>
2026-03-22 15:25:43 -0300

    Upgrade node and make jest use swc

--- jest.config.ts
+++ jest.config.ts
@@ -1,18 +1,13 @@
 import type {Config} from 'jest'
 
 const config: Config = {
-  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^@lib/(.*)$': '<rootDir>/src/lib/$1',
   },
-  preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {useESM: true},
-    ],
+    '^.+\\.tsx?$': '@swc/jest',
   },
 }
 
